b28bb30565b8446a51f3d3bf8592c1ae590c1aaf
vossie <carel.vosloo@bronzegate.com>
2025-09-15 10:54:52 +0100

    Initial

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# run.sh
+
+cd /opt/BetfairBotML
+
+# Load environment variables from set-env-vars-prod.sh
+source "$(dirname "$0")/set-env-vars-prod.sh"
+
+git pull
+.venv/bin/python -m ml.train_xgb --curated s3://betfair-curated  --sport horse-racing --date 2025-09-13 --days 8 --preoff-mins 30 --batch-markets 100 --downsample-secs 0
