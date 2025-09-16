41b9679faba1b7613ac73e17b39f8dc2857a0245
vossie <carel.vosloo@bronzegate.com>
2025-09-16 09:17:47 +0100

    Initial

--- bin/run-simulator.sh
+++ bin/run-simulator.sh
@@ -5,4 +5,24 @@ cd /opt/BetfairBotML || exit 1
 # Load environment
 source "$(dirname "$0")/set-env-vars-prod.sh"
 
-.venv/bin/python -m ml.sim   --model-30 model_30.json   --model-180 model_180.json   --gate-mins 45   --curated /mnt/nvme/betfair-curated   --sport horse-racing   --date 2025-09-14 --days 10   --preoff-mins 180   --min-edge 0.02   --kelly 0.25   --commission 0.02   --top-n-per-market 1   --side auto   --bets-out bets_dual.csv
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <DATE: YYYY-MM-DD>"
+  exit 1
+fi
+
+TARGET_DATE="$1"
+
+.venv/bin/python -m ml.sim \
+  --model-30 ../output/model_30.json \
+  --model-180 ../output/model_180.json \
+  --gate-mins 45 \
+  --curated /mnt/nvme/betfair-curated \
+  --sport horse-racing \
+  --date "$TARGET_DATE" --days 10 \
+  --preoff-mins 180 \
+  --min-edge 0.02 \
+  --kelly 0.25 \
+  --commission 0.02 \
+  --top-n-per-market 1 \
+  --side auto \
+  --bets-out bets_dual.csv
